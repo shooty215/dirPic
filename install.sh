ae3bab44efca74d91d38520fcd83410c81fa7ff6
Shooty215 <b.heinr@web.de>
2021-09-08 19:30:20 +0200

    add progress notifications to install script

--- install.sh
+++ install.sh
@@ -16,6 +16,16 @@
 
 
 ### recieve script's parameters and initialize magic variables
+# progress notifications
+PROGRESS_NOTIFICATION_CREATE_FILE_STRINGS = "Binding services' starting file contents!\n"
+PROGRESS_NOTIFICATION_CREATE_USER = "Creating user!\n"
+PROGRESS_NOTIFICATION_CREATE_SUDOERS_ENTRY = "Creating entry in sudoers file!\n"
+PROGRESS_NOTIFICATION_CLONE_GIT_REPOSITORIES = "Cloning subscriber's and publisher's git repositories!\n"
+PROGRESS_NOTIFICATION_CREATE_BINARIES = "Creating services' binary files!\n"
+PROGRESS_NOTIFICATION_MOVE_FILES = "Moving all relevant key, certificate, config and binary files!\n"
+PROGRESS_NOTIFICATION_GIVE_PRIVS_TO_APP_USER = "Giving application's folder to application's user!\n"
+PROGRESS_NOTIFICATION_ECHO_USER_CREDENTIALS = "Creating file with application's user credentials (in /home/dirpic/encrypt)!\n"
+
 # app user information
 APP_USER="dirpic"
 APP_USER_PASSWORD_PLAIN_TEXT=$(/usr/bin/openssl rand 1000 | strings | grep -io [[:alnum:]] | head -n 16 | tr -d '\n')
@@ -34,6 +44,7 @@ APP_CAMERA_DIRECTORY=$APP_USER_HOME_DIRECTORY"camera/"
 APP_STORAGE_DIRECTORY=$APP_USER_HOME_DIRECTORY"storage/"
 
 # files and hyper links
+MOTION_CONFIG = "~/dirPic/motion.conf"
 GIT_BINARY_SUBSCRIBER_LINK="https://github.com/shooty215/dirPicSubscriber.git"
 GIT_BINARY_PUBLISHER_LINK="https://github.com/shooty215/dirPicPublisher.git"
 
@@ -72,6 +83,7 @@ BROKER_USER_PASSWORD=$5
 CA_PASSWORD=$6
 
 ### create file strings
+echo $PROGRESS_NOTIFICATION_CREATE_FILE_STRINGS
 APP_BINARY_SUBSCRIBER_START_SCRIPT="
 #!/bin/bash\n
 /usr/bin/sudo /usr/bin/java -jar $APP_BINARY_SUBSCRIBER $BROKER_IP $BROKER_PORT $BROKER_CHANNEL $APP_STORAGE_DIRECTORY $APP_KEYSTORE_DIRECTORY $BROKER_USER $BROKER_USER_PASSWORD $CA_PASSWORD\n
@@ -83,6 +95,7 @@ APP_BINARY_PUBLISHER_START_SCRIPT="
 "
 
 ### create user
+echo $PROGRESS_NOTIFICATION_CREATE_USER
 /usr/bin/sudo /usr/sbin/useradd -p $APP_USER_PASSWORD_SHA256_HASH $APP_USER -r -d $APP_USER_HOME_DIRECTORY
 #/usr/bin/sudo /usr/sbin/useradd -r -m
 ### make app user sudoer only for /bin/java
@@ -90,10 +103,12 @@ APP_BINARY_PUBLISHER_START_SCRIPT="
 /usr/bin/sudo /usr/sbin/usermod -a -G sudo $APP_USER
 
 # modify app user's sudo privs, restricting it to only use /bin/java in sudo context
+echo $PROGRESS_NOTIFICATION_CREATE_SUDOERS_ENTRY
 /usr/bin/sudo /bin/echo $APP_USER_PRIV_SUDOERS_STRING >> /etc/sudoers
 
 ### create app structure
 # create directories
+echo $PROGRESS_NOTIFICATION_CREATE_DIRECTORIES
 /usr/bin/sudo /bin/mkdir $APP_USER_HOME_DIRECTORY
 #/usr/bin/sudo /bin/mkdir $APP_ENV_ROOT_DIRECTORY
 #/usr/bin/sudo /bin/mkdir $APP_ENV_DIRECTORY
@@ -105,7 +120,8 @@ APP_BINARY_PUBLISHER_START_SCRIPT="
 /usr/bin/sudo /bin/mkdir $APP_STORAGE_DIRECTORY
 
 ## clone git repositories
-/usr/bin/sudo /usr/bin/git -C $APP_USER_HOME_DIRECTORY clone $GIT_BINARY_PUBLISHER_LINK 
+echo $PROGRESS_NOTIFICATION_CLONE_GIT_REPOSITORIES
+/usr/bin/sudo /usr/bin/git -C $APP_USER_HOME_DIRECTORY clone $GIT_BINARY_PUBLISHER_LINK
 /usr/bin/sudo /usr/bin/git -C $APP_USER_HOME_DIRECTORY clone $GIT_BINARY_SUBSCRIBER_LINK 
 
 ## copy binary files from git repository to app directory
@@ -116,6 +132,7 @@ APP_BINARY_PUBLISHER_START_SCRIPT="
 
 ### create and copy binary files
 # create file not needed due to > operators functionality (creates the file)
+echo $PROGRESS_NOTIFICATION_CREATE_BINARIES
 /usr/bin/sudo /bin/touch $APP_BINARY_SUBSCRIBER_START
 /usr/bin/sudo /bin/touch $APP_BINARY_PUBLISHER_START
 
@@ -126,6 +143,8 @@ APP_BINARY_PUBLISHER_START_SCRIPT="
 # turn shell files into binaries
 /usr/bin/sudo /usr/bin/shc -f $APP_BINARY_PUBLISHER_START -o $APP_BINARY_PUBLISHER_START_ACTUAL
 /usr/bin/sudo /usr/bin/shc -f $APP_BINARY_SUBSCRIBER_START -o $APP_BINARY_SUBSCRIBER_START_ACTUAL
+
+echo $PROGRESS_NOTIFICATION_MOVE_FILES
 
 # move binary shell files to /usr/bin/
 /usr/bin/sudo /bin/mv $APP_BINARY_PUBLISHER_START_ACTUAL /usr/bin/${APP_BINARY_PUBLISHER_START_FILENAME}
@@ -140,12 +159,16 @@ APP_BINARY_PUBLISHER_START_SCRIPT="
 /usr/bin/sudo /bin/cp client_crt.pem /home/dirpic/keystores/client_crt.pem
 /usr/bin/sudo /bin/cp client_key.pem /home/dirpic/keystores/client_key.pem
 
+# copy motion config
+/usr/bin/sudo /bin/cp $MOTION_CONFIG /home/dirpic/
 ### set privs, ownership and group of app user's home directory, the service and the binary files
 # maybe not the service and the binary files
 # home directory
+echo $PROGRESS_NOTIFICATION_GIVE_PRIVS_TO_APP_USER
 /usr/bin/sudo /bin/chmod -R 750 /home/dirpic/
 /usr/bin/sudo /bin/chown -R dirpic /home/dirpic/
 /usr/bin/sudo /bin/chgrp -R dirpic /home/dirpic/
 
 ### echo user password into file
+echo $PROGRESS_NOTIFICATION_ECHO_USER_CREDENTIALS
 /usr/bin/sudo /bin/echo $APP_USER_PASSWORD_PLAIN_TEXT'|:::::|'$APP_USER_PASSWORD_SHA256_HASH > $APP_USER_HOME_DIRECTORY'encrypt'
